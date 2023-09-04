76fdc9568471b49a8564b66660a324f92de81c61
Subramanian Kumarappan <Subramanian.Kumarappan@halliburton.com>
2023-09-04 11:11:40 +0530

    first commit

---
+++ SigTermExit/SigTermExit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SigTermExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
